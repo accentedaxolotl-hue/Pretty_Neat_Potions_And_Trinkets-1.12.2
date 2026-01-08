12a6d38c59e114fd9ed10b1261b08de5eaa6c265
Tsukamoto <tsukamototheeditor@gmail.com>
2026-01-08 15:35:35 +0500

    Still crashes

--- src/main/java/net/itwaskairo/lepton/Lepton.java
+++ src/main/java/net/itwaskairo/lepton/Lepton.java
@@ -8,22 +8,28 @@ import net.minecraftforge.client.event.ModelRegistryEvent;
 import net.minecraftforge.client.model.ModelLoader;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
-@Mod(modid = Lepton.MODID, name = "Lepton", version = "1.0", acceptableRemoteVersions = "*")
+@Mod(
+        modid = "lepton",
+        name = "Lepton",
+        version = "1.0",
+        acceptableRemoteVersions = "*"
+)
 public class Lepton {
     public static final String MODID = "lepton";
-
-    @Mod.Instance(MODID)
-    public static Lepton instance;
 
     @Mod.EventHandler
     public void preInit(FMLPreInitializationEvent event) {
 
         ModItems.preInit();
         ModEffects.preInit();
+    }
 
+    @Mod.EventHandler
+    public void init(FMLInitializationEvent event) {
         MinecraftForge.EVENT_BUS.register(new MobSetup());
     }
 
